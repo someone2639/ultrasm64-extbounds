462d0274a42a6082f7a1145e54e403892f41959f
someone2639 <someone2639@gmail.com>
2023-07-09 01:45:42 -0400

    add instructions

--- src/game/Contpak.c
+++ src/game/Contpak.c
@@ -9,6 +9,11 @@
 #include "debug.h"
 #include "game_init.h"
 
+// INSTRUCTIONS
+//  Step 1: Go to levels/(your level)/model.inc.c and find your texture
+//  Step 2: Extern that symbol here and add it to the texture array
+//           [LEVEL_XYZ] = &your_texture,
+
 extern void *castle_grounds_seg7_texture_07002000;
 
 void *cpr_textureArray[LEVEL_COUNT] = {
@@ -17,12 +22,15 @@ void *cpr_textureArray[LEVEL_COUNT] = {
 
 
 
+
+
+
+
 void cpr_copytex(u32 *p) {
     void *dst = segmented_to_virtual(p);
     memcpy(dst, cpr_Texture, sizeof(cpr_Texture));
 }
 void cpr_apply() {
-
     if (cpr_textureArray[gCurrLevelNum] != 0) {
         cpr_copytex(cpr_textureArray[gCurrLevelNum]);
     }
