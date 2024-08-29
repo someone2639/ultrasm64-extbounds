92dc37dff33d7fee592ceac1cf8f76a461cba0c0
someone2639 <someone2639@gmail.com>
2024-08-28 20:07:20 -0400

    poll results from #sm64-pr-discussion: it's now cutscene_helpers

--- src/game/object_helpers.c
+++ src/game/object_helpers.c
@@ -5,7 +5,7 @@
 #include "behavior_actions.h"
 #include "behavior_data.h"
 #include "camera.h"
-#include "camera/camera_cutscene.h"
+#include "camera/cutscene_helpers.h"
 #include "debug.h"
 #include "debugger/assert.h"
 #include "dialog_ids.h"
