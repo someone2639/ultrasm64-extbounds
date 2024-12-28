a73228d45c9178087f72e96b31cc19a04f0bc2c1
someone2639 <someone2639@gmail.com>
2024-12-28 12:16:25 -0500

    update goddard size script to work on any build folder (allows HackerSM64 to be used as a submodule on the template repo)

--- tools/getGoddardSize.py
+++ tools/getGoddardSize.py
@@ -1,12 +1,14 @@
-import sys, os
+import sys
 
-with open(sys.argv[1]) as f:
+# Usage:
+#  getGoddardSize.py <build_dir/> <.map file>
+
+with open(sys.argv[2]) as f:
 	for line in f:
 		if "GODDARD_SIZE" in line:
 			tokens=line.split()
-			with open("build/%s_n64/goddard.txt" % sys.argv[2], "w+") as f:
+			with open("%s/goddard.txt" % sys.argv[1], "w+") as f:
 				sz = int(tokens[0], 16)
 				sz += 16
 				sz &= 0xFFFFFFF0
 				f.write("GODDARD_SIZE = 0x%X;" % sz)
-				
