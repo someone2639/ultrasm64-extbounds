f47acdf997c9b8efacfd0b3bfdc2421eaa287993
someone2639 <someone2639@gmail.com>
2024-12-28 11:35:40 -0500

    nop out asserts under recomp

--- asm/n64_assert.s
+++ asm/n64_assert.s
@@ -11,9 +11,20 @@ glabel __n64Assert_Message
 .section .text
 
 glabel __n64Assert
+.type __n64Assert, @function
+#ifndef RECOMP
 sw $a0, __n64Assert_Filename
 sw $a1, __n64Assert_LineNum
 sw $a2, __n64Assert_Message
 syscall
-nop
-
+ nop
+#else // RECOMP
+/*
+This is an unusual function to throw through recomp,
+ so we make asserts a no-op.
+ Fix your bugs before porting.
+*/
+jr $ra
+ nop
+#endif // RECOMP
+.size __n64Assert, . - __n64Assert
