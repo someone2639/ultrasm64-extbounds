4599bd52e5e17f002394f44823bfa975d26f0980
someone2639 <someone2639@gmail.com>
2026-09-21 07:40:13 -0400

    everything in a separate static DL works

--- src/goddard/dynlists/dynlist_stars.c
+++ src/goddard/dynlists/dynlist_stars.c
@@ -12,6 +12,17 @@ struct DynList dynlist_stars[] = {
 
     StartGroup(234),
 
+        MakeDynObj(D_LIGHT, DYNOBJ_SILVER_STAR_LIGHT),
+            SetId(1),
+            SetDiffuse(1.0, 1.0, 1.0),
+            SetFlag(0x20),
+            SetShapePtrPtr(&gShapeSilverStar),
+
+        MakeDynObj(D_LIGHT, DYNOBJ_RED_STAR_LIGHT),
+            SetId(0),
+            SetDiffuse(1.0, 0.0, 0.0),
+            SetShapePtrPtr(&gShapeRedStar),
+
         // silver star animator
         MakeDynObj(D_DATA_GRP, DYNOBJ_SILVER_STAR_ANIMDATA_GROUP),
             LinkWithPtr(&anim_silver_star),
@@ -23,10 +34,10 @@ struct DynList dynlist_stars[] = {
         // red star animator
         MakeDynObj(D_DATA_GRP, DYNOBJ_RED_STAR_ANIMDATA_GROUP),
             LinkWithPtr(&anim_red_star),
-        MakeDynObj(D_ANIMATOR, DYNOBJ_RED_STAR_ANIMATOR),
-            AttachTo(0x0, DYNOBJ_MARIO_MAIN_ANIMATOR),
-            SetNodeGroup(DYNOBJ_RED_STAR_ANIMDATA_GROUP),
-            LinkWith(DYNOBJ_RED_STAR_LIGHT),
+            MakeDynObj(D_ANIMATOR, DYNOBJ_RED_STAR_ANIMATOR),
+                AttachTo(0x0, DYNOBJ_MARIO_MAIN_ANIMATOR),
+                SetNodeGroup(DYNOBJ_RED_STAR_ANIMDATA_GROUP),
+                LinkWith(DYNOBJ_RED_STAR_LIGHT),
 
     EndGroup(234),
 
